8136b4679c98d40d3dc18fe0c87644a48fd5d05d
sttelite <138879763+sttelite@users.noreply.github.com>
2023-08-19 05:45:08 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,6 +1,26 @@
 using System;
-class Program {
-  static void Main() {
-    Console.WriteLine("1");
-  }
+using System.Collections.Generic;
+
+namespace LanguageRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Language Recognition Program!");
+            Console.WriteLine("Enter the text you want to recognize:");
+
+            string inputText = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(inputText))
+            {
+                string detectedLanguage = RecognizeLanguage(inputText);
+                Console.WriteLine($"Detected Language: {detectedLanguage}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter some text.");
+            }
+        }
+    }
 }
